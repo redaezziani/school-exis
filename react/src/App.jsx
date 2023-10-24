a5825d8dc2c291e163b77a5b4645fa27ffa8342f
redaezziani <2003100500167@eofppt-edu.ma>
2023-10-24 10:27:36 +0100

    add

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,10 +1,7 @@
-
-import Header from './components/Header';
-import Presontation from './classComponents/Presontation';
-import Vioture from "./classComponents/Vioture";
 import { motion } from 'framer-motion';
 import './css/App.css'
 import img from './assets/tesla.jpg'
+import Button from "@mui/material/Button";
 
 function App() {
   const data = {
@@ -18,22 +15,24 @@ function App() {
   return (
     <div className="w-full flex-col gap-3  h-screen flex justify-center items-center ">
       <motion.div className="card flex flex-col gap-2  w-96 justify-start overflow-hidden  items-center  ">
-          <motion.img
-          className='w-full h-52 object-cover aspect-square rounded-md '
-          src={
-            data.img
-          } alt="image" />
-          <div className="text-content flex w-full mt-4 flex-col gap-2 ">
-            <h1 className="text-2xl font-bold text-slate-900">
-              {data.name}
-            </h1>
-            <p className="text-slate-900 text-sm">
-              {data.des}
-            </p>
-            <span>
-              <a href="https://en.wikipedia.org/wiki/Nikola_Tesla" target="_blank" rel="noopener noreferrer" className="text-blue-500 font-bold text-sm">Read more</a>
-            </span>
+        <motion.img
+          className="w-full h-52 object-cover aspect-square rounded-md "
+          src={data.img}
+          alt="image"
+        />
+        <div className="text-content flex w-full mt-4 flex-col gap-2 ">
+          <h1 className="text-2xl font-bold text-slate-900">{data.name}</h1>
+          <p className="text-slate-900 text-sm">{data.des}</p>
+          <div className="flex">
+            <Button
+              variant="outlined"
+              size="medium"
+               href="https://en.wikipedia.org/wiki/Nikola_Tesla"
+            >
+              Read More
+            </Button>
           </div>
+        </div>
       </motion.div>
     </div>
   );
